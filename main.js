5500c42527fd3a98cfd3a05375ca0e9f8778289f
Juan Diego Soto <hornodelaabue@gmail.com>
2024-06-17 12:06:20 -0500

    update

--- main.js
+++ main.js
@@ -6,16 +6,29 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 
 let contador = 0
 
-while (contador < 9){
-   const tecla = listaDeTeclas[contador];
-   const instrumento = tecla.classList[1];
+for(let contador = 0; contador < listaDeTeclas.length; contador = contador++){
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+ 
+    const idAudio = `#sonido_${instrumento}`;
+    console.log(idAudio)
+ 
+    tecla.onclick = function (){
+     playSonido(idAudio);
+    }; 
 
-   const idAudio = `#sonido_${instrumento}`;
-   console.log(idAudio)
+    contador = contador + 1;
 
-   tecla.onclick = function (){
-    playSonido(idAudio);
-   }; 
-   contador = contador + 1;
-   
+    tecla.onkeydown = function(){
+        if ('espacio') {
+            
+        } else {
+            
+        }
+        tecla.classList.add('activa');
+        
+        tecla.onkeyup = function(){
+            tecla.classList.remove('activa');
+        }
+    }
 }
