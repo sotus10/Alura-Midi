5917017b23f4ba03883cfc9b81906f566d88be68
Juan Diego Soto <hornodelaabue@gmail.com>
2024-06-17 13:17:46 -0500

    Final

--- main.js
+++ main.js
@@ -4,31 +4,30 @@ function playSonido (idElementoAudio){
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
-let contador = 0
 
-for(let contador = 0; contador < listaDeTeclas.length; contador = contador++){
+for(let contador = 0; contador < listaDeTeclas.length; contador++){
     const tecla = listaDeTeclas[contador];
     const instrumento = tecla.classList[1];
- 
+    console.log(contador)
+
     const idAudio = `#sonido_${instrumento}`;
     console.log(idAudio)
- 
+
     tecla.onclick = function (){
-     playSonido(idAudio);
-    }; 
-
-    contador = contador + 1;
-
-    tecla.onkeydown = function(){
-        if ('espacio') {
-            
-        } else {
-            
-        }
-        tecla.classList.add('activa');
+        playSonido(idAudio);
+    };
+    tecla.onkeydown = function(evento){
         
-        tecla.onkeyup = function(){
-            tecla.classList.remove('activa');
+        if (evento.code === 'Space' || evento.code === 'Enter'){
+            tecla.classList.add('activa');
         }
+        console.log(evento.code === 'Space' || evento.code === 'Enter')
+       
+        
     }
+    tecla.onkeyup = function(){
+        tecla.classList.remove('activa');
+    }
+   
+
 }
